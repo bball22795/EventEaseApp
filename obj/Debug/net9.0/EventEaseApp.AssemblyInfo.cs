d7baffca33dc74219d61616c8e4357c359d2a09c
Ronny <waingortr@gmail.com>
2025-03-11 15:13:22 -0400

    Initial commit of EventEaseApp

--- obj/Debug/net9.0/EventEaseApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/EventEaseApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventEaseApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+702d9910e9c3cdca6e5331ca6aa83c992810ce3a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96022e55f2630094f50ff397573afc8684b5ad9f")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventEaseApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventEaseApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
